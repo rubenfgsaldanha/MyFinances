d8e7bcfca6973303aa111e251f660832786e55de
Rúben <rfgsaldanha@gmail.com>
2018-11-16 08:10:39 +0000

    updated

--- app/src/main/java/pt/uc/dei/cm/myfinances/SignedInActivity.java
+++ app/src/main/java/pt/uc/dei/cm/myfinances/SignedInActivity.java
@@ -3,6 +3,7 @@ package pt.uc.dei.cm.myfinances;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import pt.uc.dei.cm.myfinances.myfinances.R;
@@ -11,34 +12,24 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.util.ExtraConstants;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthProvider;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.UserInfo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class SignedInActivity extends AppCompatActivity {
 
@@ -69,11 +60,80 @@ public class SignedInActivity extends AppCompatActivity {
             return;
         }
 
+        welcomeText.setText("Welcome, "+currentUser.getDisplayName());
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu,menu);
+
+        //Our super action bar also has a new icon
+        ActionBar ab = getSupportActionBar();
+
+        //change the app icon in the app bar
+        ab.setHomeAsUpIndicator(R.drawable.ic_financeslogo);
+
+        //Show the icon - selecting "home" returns a single level
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        //ab.setTitle("ViewPager Example");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.drive_sync:
+                Toast.makeText(getApplicationContext(),"Drive sync",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_bar_export:
+                Toast.makeText(getApplicationContext(),"Export",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_bar_about:
+                Toast.makeText(getApplicationContext(),"About",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void showSnackbar(@StringRes int errorMessageRes) {
         Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void askSignOut(){
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to log out?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        signOut();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                startActivity(LoginActivity.createIntent(SignedInActivity.this));
+                                finish();
+                            } else {
+                                Log.w(TAG, "signOut:failure", task.getException());
+                                showSnackbar(R.string.sign_out_failed);
+                            }
+                        }
+                );
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -92,24 +152,9 @@ public class SignedInActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.navigation_log_off:
-                        signOut();
+                        askSignOut();
                         return true;
                 }
                 return false;
             };
-
-    public void signOut() {
-        AuthUI.getInstance()
-                .signOut(this)
-                .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                startActivity(LoginActivity.createIntent(SignedInActivity.this));
-                                finish();
-                            } else {
-                                Log.w(TAG, "signOut:failure", task.getException());
-                                showSnackbar(R.string.sign_out_failed);
-                            }
-                        }
-                );
-    }
 }
