193e5d17c9fa2351588b17fc0b6b2a94139098c6
Rúben <rfgsaldanha@gmail.com>
2018-11-22 08:26:13 +0000

    master up to date

--- app/src/main/java/pt/uc/dei/cm/myfinances/fragments/HomeFragment.java
+++ app/src/main/java/pt/uc/dei/cm/myfinances/fragments/HomeFragment.java
@@ -1,26 +1,51 @@
 package pt.uc.dei.cm.myfinances.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import pt.uc.dei.cm.myfinances.MyFinancesApplication;
+import pt.uc.dei.cm.myfinances.activities.AddTransactionActivity;
+import pt.uc.dei.cm.myfinances.adapters.TransactionAdapter;
+import pt.uc.dei.cm.myfinances.general.Transaction;
+import pt.uc.dei.cm.myfinances.general.Wallet;
 import pt.uc.dei.cm.myfinances.myfinances.R;
 
+import static android.app.Activity.RESULT_OK;
 
-public class HomeFragment extends androidx.fragment.app.Fragment {
+
+public class HomeFragment extends androidx.fragment.app.Fragment implements AdapterView.OnItemClickListener {
     private static final String TAG = "HomeFragment";
+    private static final int START_ACT_CODE = 1000;
     private OnFragmentInteractionListener mListener;
 
-    @BindView(R.id.fab_add_item)
-    FloatingActionButton fab_add_item;
+    @BindView(R.id.fragmentHome) View mRootView;
+    @BindView(R.id.recycle_view_transactions) RecyclerView transactionsList;
+    @BindView(R.id.fab_add_transaction) FloatingActionButton fabAddTransaction;
+    @BindView(R.id.balance) TextView balance;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private TransactionAdapter adapter;
+
+    MyFinancesApplication app;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,21 +54,61 @@ public class HomeFragment extends androidx.fragment.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //
+        //use savedInstanceState?
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootView);
+
+        app = (MyFinancesApplication) getActivity().getApplicationContext();
+        if(app.getWallets().size() == 0){
+            defaultWallet();
+        }
+
+        return rootView;
     }
 
-    @OnClick(R.id.fab_add_item)
-    public void addItem(){
-        Toast.makeText(getContext(),"Now we had something",Toast.LENGTH_SHORT).show();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // specify an adapter (see also next example)
+        adapter = new TransactionAdapter(getContext(),this, app.getCurrentWallet().getTransactions());
+        transactionsList.setAdapter(adapter);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        transactionsList.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        transactionsList.setLayoutManager(mLayoutManager);
     }
-    
+
+    //TODO Comunicar com BD para saber as wallets existentes
+    //for now create a default wallets
+    private void defaultWallet(){
+        Wallet wallet1 =  new Wallet("Wallet1",0);
+        app.getWallets().add(wallet1);
+        app.setCurrentWallet(wallet1);
+    }
+
+    @OnClick(R.id.fab_add_transaction)
+    public void addItem(){
+        Intent startAddTransaction =  new Intent(getActivity(), AddTransactionActivity.class);
+        startActivityForResult(startAddTransaction, START_ACT_CODE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+        balance.setText(getString(R.string.balance)+" "+app.getCurrentWallet().getBalance());
+    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -69,16 +134,22 @@ public class HomeFragment extends androidx.fragment.app.Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity().getApplicationContext(),"Item "+position+" clicked",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if(requestCode == START_ACT_CODE){
+                Snackbar.make(mRootView, R.string.add_trans_suc, Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //Allow fragment to communicate with activity and other fragments
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
