c67ade13ef1032c5125a2c74852d2a6d1f607959
Admin <adrimontcynha@gmail.com>
2018-12-27 19:44:08 +0000

    cant display label color

--- app/src/main/java/pt/uc/dei/cm/myfinances/adapters/CategoryAdapter.java
+++ app/src/main/java/pt/uc/dei/cm/myfinances/adapters/CategoryAdapter.java
@@ -62,6 +62,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         this.onItemClickListener = onItemClickListener;
     }
 
+
     @Override
     public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 // create a new view
